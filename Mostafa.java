0a496c8e8a33e103d8d670511774063cc367347a
root <mostafa.zahra69320@gmail.com>
2022-11-07 01:40:42 +0200

    master

---
+++ Mostafa.java
@@ -0,0 +1,8 @@
+public class Mostafa {  
+
+    public static void main(String[] args) {
+
+    System.out.print("Mostafa Ali Ahmed Zahra\n");
+    }
+    
+}
---
+++ dockerfile
@@ -0,0 +1,6 @@
+FORM openjdk
+WORKDIR /DOWNLOUD
+COPY Mostafa.java .
+RUN javac Mostafa.java
+
+CMD java Mostafa
